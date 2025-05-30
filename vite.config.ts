1acf8884017a0819d7e8596f31e56777dacda9d5
carlosf1809 <carlosmoreiraaraujo18@gmail.com>
2025-05-30 19:06:26 -0300

    alt

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react      from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // troque totem-paypal pelo nome do seu repositório
-  base: '/totem-paypal/',
+  base: '/',
 })
